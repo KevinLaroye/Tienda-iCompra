9ab31f11eaf3e59fa9a142ee1da22a0d9dc9350f
kaeleb <kevinzk55@gmail.com>
2022-12-12 22:29:01 -0300

    Pre entrega 2

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function procesoDeCompra(){
+/* function procesoDeCompra(){
     
 let compra = "";
 let total = 0;
@@ -113,4 +113,69 @@ if (edad < 18) {
                 break;
         }
     } while (compra != "no");
-}}
+}} */
+
+// Pre-entrega 2
+
+const inventario = [
+    { producto: "iPhone 11", precio: 400 },
+    { producto: "iPhone 12", precio: 550 },
+    { producto: "iPhone 13", precio: 700 },
+    { producto: "iPhone 14", precio: 800 },
+    { producto: "iPad 3", precio: 400 },
+    { producto: "iPad 4", precio: 500 },
+    { producto: "iPad 5", precio: 600 },
+    { producto: "iPad 6", precio: 700 },
+    { producto: "iMac 2019", precio: 600 },
+    { producto: "iMac 2020", precio: 800 },
+    { producto: "iMac 2021", precio: 1000 },
+    { producto: "iMac 2022", precio: 1200 }
+]
+
+let opcion;
+
+function filtrado(prod) {
+    const inventarioNew = inventario.filter(item => item.producto.includes(prod))
+    inventarioNew.forEach(producto => console.log(`Producto: ${producto.producto}. Precio: $${producto.precio}`))
+    alert(`El Inventario de ${prod} se ha impreso en la consola`)
+    opcion = prompt(`¿Te gustaría hacer otra consulta?
+                    - Escribe "si" si te gustaría hacer otra consulta
+                    - Escribe "salir" para finalizar`)
+}
+
+alert("Bienvenido a tu tienda iCompra")
+
+
+do {
+    opcion = prompt(`¿Que te gustaría consultar?
+1. Inventario Completo
+2. iPhones
+3. iPads
+4. iMacs
+5. Para salir escriba "salir"`)
+
+
+switch (opcion) {
+    case "1":
+        inventario.forEach(producto => console.log(`Producto: ${producto.producto}. Precio: $${producto.precio}`))
+        alert(`El Inventario se ha impreso en la consola`)
+        opcion = prompt(`¿Te gustaría hacer otra consulta?
+                    - Escribe "si" si te gustaría hacer otra consulta
+                    - Escribe "salir" para finalizar`)
+        break;
+    case "2":
+        filtrado("iPhone")
+        break;
+    case "3":
+        filtrado("iPad")
+        break;
+    case "4":
+        filtrado("iMac")
+        break;
+        case "salir":
+            break;
+    default:
+        alert(`Por favor seleccione una de las opciones anteriores`)
+        break;
+}
+} while (opcion != "salir");
