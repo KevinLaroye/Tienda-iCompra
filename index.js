a6c996494fe5ae8128751419ef4263d6fae0ad9d
kaeleb <kevinzk55@gmail.com>
2022-12-29 02:02:43 -0300

    Pre Entrega 3

--- index.js
+++ index.js
@@ -1,181 +1,73 @@
-/* function procesoDeCompra(){
+import inventario from './data.json' assert {type: 'json'}
+const barraDeBusqueda = document.querySelector("#barraDeBusqueda")
+const productos = document.querySelector("#productos")
+const cantidadCarrito = document.querySelector(".badge")
+let carritoDeCompras = JSON.parse(localStorage.getItem("data")) || []
+cantidadCarrito.innerHTML = carritoDeCompras.length
+
+function crearTarjeta(inventario){
+    inventario.forEach((producto) => {
+        
+        const nuevoDiv = document.createElement('div')
+        
+        nuevoDiv.className = "col mb-5"
     
-let compra = "";
-let total = 0;
-let stockIphone = 10;
-let stockIpad = 8;
-let stockImac = 5;
-let producto = "";
-const precioIphone = 1000;
-const precioIpad = 2000;
-const precioImac = 5000;
-
-function calculoCompra(articuloCompra, producto, precio, stock) {
-    alert(
-        `Usted quiere comprar ${articuloCompra} ${producto}(s) por un total de $${articuloCompra * precio
-        }`
-    );
+        nuevoDiv.innerHTML = `<div class="card h-100>
+        <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+        <div class="card-body p-4">
+            <div class="text-center">
+                <h5 class="itemNombre fw-bolder">${producto.producto}</h5>
+                <p class="itemPrecio">$${producto.precio}</p>
+            </div>
+        </div>
+        <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+            <div class="text-center"><btn class="addToCart btn btn-outline-dark mt-auto" href="#">Add to cart</btn></div>
+        </div>
+        </div>
+        `
+    
+        productos.appendChild(nuevoDiv)
+    })
 }
 
-alert("Bienvenido a tu tienda iCompra");
-let edad = parseInt(prompt("Por favor ingrese su edad:"));
+crearTarjeta(inventario)
 
-if (edad < 18) {
-    alert("Debes ser mayor de edad para poder comprar");
-} else {
-    do {
-        let articulo = prompt(`¿Qué te gustaría comprar?
-     1. iPhone
-     2. iPad
-     3. iMac`);
+barraDeBusqueda.addEventListener('input', (e) => {
+    const searchInput = e.target.value.toLowerCase()
+    
+    productos.innerHTML = ""
 
-        switch (articulo) {
-            case "1":
-                alert(`El precio del iPhone es de $1000.
-                        Actualmente hay ${stockIphone} en stock`);
+    const inventarioNew = inventario.filter(producto => producto.producto.toLowerCase().includes(searchInput))
 
-                let iphoneCompra = parseInt(
-                    prompt("Ingrese la cantidad de iPhones que desea comprar")
-                );
+    crearTarjeta(inventarioNew)
 
-                calculoCompra(iphoneCompra, "iPhone", precioIphone, stockIphone);
+})
 
-                stockIphone -= iphoneCompra;
 
-                total += iphoneCompra * precioIphone;
+const addToCart = document.querySelectorAll(".addToCart")
+console.log(addToCart)
 
-                compra = prompt(
-                    "Desea comprar otro artículo? En caso contrario por favor escriba no"
-                );
+addToCart.forEach( btn => {
+    btn.addEventListener('click', e => {
 
-                if (compra != "no") {
-                    break;
-                } else {
-                    alert(`Gracias por tu compra!
-                    Tu total fue ${total}`);
-                }
-                break;
-            case "2":
-                alert(`El precio del iPad es de $2000.
-                Actualmente hay ${stockIpad} en stock`);
 
-                let ipadCompra = parseInt(
-                    prompt("Ingrese la cantidad de iPads que desea comprar")
-                );
+        let botonPresionado = e.target
+        let itemComprado = botonPresionado.parentElement.parentElement.parentElement
+        let itemCompradoNombre = itemComprado.getElementsByClassName("itemNombre")[0].innerText
+        let itemCompradoPrecio = itemComprado.getElementsByClassName("itemPrecio")[0].innerText
+        console.log(itemCompradoNombre)
+        console.log(itemCompradoPrecio)
 
-                calculoCompra(ipadCompra, "iPad", precioIpad, stockIpad);
-
-                stockIpad -= ipadCompra;
-
-                total += ipadCompra * precioIpad;
-
-                compra = prompt(
-                    "Desea comprar otro artículo? En caso contrario por favor escriba no"
-                );
-
-                if (compra != "no") {
-                    break;
-                } else {
-                    alert(`Gracias por tu compra!
-            Tu total fue ${total}`);
-                }
-                break;
-            case "3":
-                alert(`El precio del iMac es de $5000.
-                Actualmente hay ${stockImac} en stock`);
-
-                let imacCompra = parseInt(
-                    prompt("Ingrese la cantidad de iMacs que desea comprar")
-                );
-
-                calculoCompra(imacCompra, "iMac", precioImac, stockImac);
-
-                stockImac -= imacCompra;
-
-                total += imacCompra * precioImac;
-
-                compra = prompt(
-                    "Desea comprar otro artículo? En caso contrario por favor escriba no"
-                );
-
-                if (compra != "no") {
-                    break;
-                } else {
-                    alert(`Gracias por tu compra!
-            Tu total fue ${total}`);
-                }
-                break;
-            default:
-                alert(
-                    "Por favor seleccione una de las opciones anteriores o escriba (no) para salir"
-                );
-                compra = prompt("¿Desea continuar su compra? Si/No").toLowerCase();
-                break;
+        const productoComprado = {
+            producto: itemCompradoNombre,
+            precio: itemCompradoPrecio
         }
-    } while (compra != "no");
-}} */
 
-// Pre-entrega 2
+        carritoDeCompras.push(productoComprado)
+        console.log(carritoDeCompras)
 
-const inventario = [
-    { producto: "iPhone 11", precio: 400 },
-    { producto: "iPhone 12", precio: 550 },
-    { producto: "iPhone 13", precio: 700 },
-    { producto: "iPhone 14", precio: 800 },
-    { producto: "iPad 3", precio: 400 },
-    { producto: "iPad 4", precio: 500 },
-    { producto: "iPad 5", precio: 600 },
-    { producto: "iPad 6", precio: 700 },
-    { producto: "iMac 2019", precio: 600 },
-    { producto: "iMac 2020", precio: 800 },
-    { producto: "iMac 2021", precio: 1000 },
-    { producto: "iMac 2022", precio: 1200 }
-]
+        cantidadCarrito.innerHTML = carritoDeCompras.length
 
-let opcion;
-
-function filtrado(prod) {
-    const inventarioNew = inventario.filter(item => item.producto.includes(prod))
-    inventarioNew.forEach(producto => console.log(`Producto: ${producto.producto}. Precio: $${producto.precio}`))
-    alert(`El Inventario de ${prod} se ha impreso en la consola`)
-    opcion = prompt(`¿Te gustaría hacer otra consulta?
-                    - Escribe "si" si te gustaría hacer otra consulta
-                    - Escribe "salir" para finalizar`)
-}
-
-alert("Bienvenido a tu tienda iCompra")
-
-
-do {
-    opcion = prompt(`¿Que te gustaría consultar?
-1. Inventario Completo
-2. iPhones
-3. iPads
-4. iMacs
-5. Para salir escriba "salir"`)
-
-
-switch (opcion) {
-    case "1":
-        inventario.forEach(producto => console.log(`Producto: ${producto.producto}. Precio: $${producto.precio}`))
-        alert(`El Inventario se ha impreso en la consola`)
-        opcion = prompt(`¿Te gustaría hacer otra consulta?
-                    - Escribe "si" si te gustaría hacer otra consulta
-                    - Escribe "salir" para finalizar`)
-        break;
-    case "2":
-        filtrado("iPhone")
-        break;
-    case "3":
-        filtrado("iPad")
-        break;
-    case "4":
-        filtrado("iMac")
-        break;
-        case "salir":
-            break;
-    default:
-        alert(`Por favor seleccione una de las opciones anteriores`)
-        break;
-}
-} while (opcion != "salir");
+        localStorage.setItem("data", JSON.stringify(carritoDeCompras))
+    })
+})
